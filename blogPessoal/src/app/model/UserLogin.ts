b9542f46b279fd3b7699afed6ed2689ac926ec03
Rodrigo Dias <80522229+RodrigoPDias@users.noreply.github.com>
2021-05-31 11:56:29 -0300

    rotas de entrar e cadastrar

---
+++ blogPessoal/src/app/model/UserLogin.ts
@@ -0,0 +1,9 @@
+export class UserLogin{
+  public id: number
+  public nome: string
+  public usuario: string
+  public token: string
+  public senha: number
+  public foto: string
+  public tipo: string
+}
