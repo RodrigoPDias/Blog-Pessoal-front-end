61bcf4ad3a4043f7188c94ddc3858e7ea27feb4f
Rodrigo Dias <80522229+RodrigoPDias@users.noreply.github.com>
2021-06-03 11:56:22 -0300

    get and post add

--- blogPessoal/src/app/tema/tema.component.ts
+++ blogPessoal/src/app/tema/tema.component.ts
@@ -1,6 +1,8 @@
+import { TemaService } from './../service/tema.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Tema } from '../model/Tema';
 
 @Component({
   selector: 'app-tema',
@@ -9,16 +11,32 @@ import { environment } from 'src/environments/environment.prod';
 })
 export class TemaComponent implements OnInit {
 
-
+  tema : Tema = new Tema()
+  listaTema: Tema[]
   constructor(
+    private TemaService: TemaService,
     private router : Router
   ) { }
 
   ngOnInit() {
-    /*if(environment.token == ''){
+    if(environment.token == ''){
       alert ('Sessão expirou')
       this.router.navigate(['/entrar'])
-    }*/
+    }
+    this.findAllTema()
+  }
+  findAllTema(){
+    this.TemaService.getAllTema().subscribe((resp: Tema[]) =>{
+      this.listaTema = resp
+    })
+  }
+  cadastrar(){
+    this.TemaService.postTema(this.tema).subscribe((resp: Tema) =>{
+      this.tema = resp
+      alert('tema cadastrado com sucesso!')
+      this.findAllTema()
+      this.tema = new Tema
+    })
   }
 
 }
