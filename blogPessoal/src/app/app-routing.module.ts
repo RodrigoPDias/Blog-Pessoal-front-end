db6a62fcf5ccad5a90cb517da27f0e6cfe1bee09
Rodrigo Dias <80522229+RodrigoPDias@users.noreply.github.com>
2021-06-04 16:20:34 -0300

    add delete and Edit for Tema

--- blogPessoal/src/app/app-routing.module.ts
+++ blogPessoal/src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { TemaDeleteComponent } from './delele/tema-delete/tema-delete.component';
+import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { TemaComponent } from './tema/tema.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { Component, NgModule } from '@angular/core';
@@ -11,7 +13,9 @@ const routes: Routes = [
   {path:'inicio', component:InicioComponent },
   {path:'entrar', component:EntrarComponent},
   {path:'cadastrar', component:CadastrarComponent},
-  {path:'tema',component:TemaComponent}
+  {path:'tema',component:TemaComponent},
+  {path:'tema-edit/:id',component:TemaEditComponent},
+  {path:'tema-delete/:id',component:TemaDeleteComponent}
 
 ];
 
